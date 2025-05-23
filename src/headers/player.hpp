ad39949a1727621e677da48e6f747ca9a87e7dee
TheHeroFiction <79006937+TheHeroFiction@users.noreply.github.com>
2025-05-23 08:36:27 +0000

    player update mouvement

--- src/headers/player.hpp
+++ src/headers/player.hpp
@@ -8,9 +8,9 @@ struct Player
     float coord_x {};
     float coord_y {};
     std::vector<bool> keys_state {};
+    float speed {10.f};
 
-    float speed {2.f};
-    void update_player_position(double const deltaTime);
+    void update_player_position(double const deltaTime, Map const& map);
     void render_player();
 };
 
@@ -19,6 +19,6 @@ extern Player player;
 void key_callback_player(GLFWwindow* window, int key, int scancode, int action, int mods);
 Player init_player(float start_coord_x, float start_coord_y);
 
-
+std::vector<float> player_collision_handler(std::vector<float> const& current_pos,std::vector<float> const& next_pos, Map const& map);
 
 
--- src/prog/player.cpp
+++ src/prog/player.cpp
@@ -26,9 +26,76 @@ Player init_player(float start_coord_x, float start_coord_y)
     return new_player;
 }
 
-
-void Player::update_player_position(double const deltaTime) 
+std::vector<float> player_collision_handler(std::vector<float> const& current_pos,std::vector<float> const& next_pos, Map const& map)
 {
+    std::vector<float> new_corners 
+    {
+        next_pos[0] - 0.5f, next_pos[1] + 0.5f,
+        next_pos[0] + 0.5f, next_pos[1] + 0.5f,
+        next_pos[0] + 0.5f, next_pos[1] - 0.5f,
+        next_pos[0] - 0.5f, next_pos[1] - 0.5f
+    };
+
+    std::vector<int> corner_in_cell{};
+    bool is_gonna_encounter_wall {false};
+
+    for (int i {0}; i < new_corners.size(); i+=2)
+    {
+        if ((new_corners[i] - static_cast<int>(new_corners[i])) < 0.5f)
+        {
+            corner_in_cell.push_back(static_cast<int>(new_corners[i]));
+        }
+        else
+        {
+            corner_in_cell.push_back(static_cast<int>(new_corners[i])+1);
+        }
+
+        if ((static_cast<int>(new_corners[i+1]) - new_corners[i+1]) < 0.5f)
+        {
+            corner_in_cell.push_back(static_cast<int>(new_corners[i+1]));
+        }
+        else
+        {
+            corner_in_cell.push_back(static_cast<int>(new_corners[i+1])+1);
+        }
+    }
+
+    for (int j {0}; j < corner_in_cell.size(); j+=2)
+    {
+        std::cout<< j << "corner x: " << corner_in_cell[j] << ", corner y: " << corner_in_cell[j+1] << std::endl;
+        if (next_pos[0] < -0.25f || corner_in_cell[j] > 49)
+        {
+            is_gonna_encounter_wall = true;
+            break;
+        } 
+        
+        if (next_pos[1] > 0.25f ||next_pos[1] < -49)
+        {
+            is_gonna_encounter_wall = true;
+            break;
+        }
+        
+        if (map.content[corner_in_cell[j]][-corner_in_cell[j+1]] == '#')
+        {
+            is_gonna_encounter_wall = true;
+            break;
+        }
+
+    }
+
+    if (is_gonna_encounter_wall)
+    {
+        return current_pos;
+    }
+    else 
+    {
+        return next_pos;
+    }
+}
+
+void Player::update_player_position(double const deltaTime, Map const& map) 
+{
+    std::vector<float> current_pos {coord_x,coord_y};
     std::vector<float> next_pos {coord_x,coord_y};
     if (keys_state[GLFW_KEY_W]) {
         next_pos[1] += deltaTime * speed;
@@ -42,9 +109,10 @@ void Player::update_player_position(double const deltaTime)
     if (keys_state[GLFW_KEY_D]) {
         next_pos[0] += deltaTime * speed;
     }
+
+    next_pos = player_collision_handler(current_pos,next_pos,map);
     coord_x = next_pos[0];
     coord_y = next_pos[1];
-    //_player_position = handle_collision_with_level(_player_position, next_position, _level);
 }
 
 void Player::render_player()
