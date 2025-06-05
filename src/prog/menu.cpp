17d6b2f2e8bfa6f5e1a7adb688929f70d6b22f4b
Lilian Peuron <lilianpeuron@gmail.com>
2025-06-05 16:10:12 +0200

    Rm Comments

--- src/prog/menu.cpp
+++ src/prog/menu.cpp
@@ -13,13 +13,11 @@ void drawMenu()
     glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Configure TextRenderer
     TextRenderer->ResetFont();
     TextRenderer->SetColor(SimpleText::TEXT_COLOR, SimpleText::Color::WHITE);
     TextRenderer->SetColorf(SimpleText::BACKGROUND_COLOR, 0.f, 0.f, 0.f, 0.f);
     TextRenderer->EnableBlending(true);
 
-    // Draw "Play" at center of button (0,0)
     TextRenderer->Label("Press S to Start", GL_VIEW_SIZE / 2, GL_VIEW_SIZE / 2, SimpleText::CENTER);
     TextRenderer->Label("Press O to quit", GL_VIEW_SIZE / 2, (GL_VIEW_SIZE / 2) + 20.f, SimpleText::CENTER);
 
@@ -38,13 +36,11 @@ void drawWin(const int &score, const int &time)
     glClearColor(0.f, 0.5f, 0.f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Configure TextRenderer
     TextRenderer->ResetFont();
     TextRenderer->SetColor(SimpleText::TEXT_COLOR, SimpleText::Color::BLACK);
     TextRenderer->SetColorf(SimpleText::BACKGROUND_COLOR, 0.f, 0.f, 0.f, 0.f);
     TextRenderer->EnableBlending(true);
 
-    // Draw "Play" at center of button (0,0)
     TextRenderer->Label("Press S to Start", GL_VIEW_SIZE / 2, (GL_VIEW_SIZE / 2) + 80.f, SimpleText::CENTER);
     TextRenderer->Label("Press O to quit", GL_VIEW_SIZE / 2, (GL_VIEW_SIZE / 2) + 100.f, SimpleText::CENTER);
 
@@ -72,13 +68,11 @@ void drawLose()
     glClearColor(0.5f, 0.f, 0.f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Configure TextRenderer
     TextRenderer->ResetFont();
     TextRenderer->SetColor(SimpleText::TEXT_COLOR, SimpleText::Color::BLACK);
     TextRenderer->SetColorf(SimpleText::BACKGROUND_COLOR, 0.f, 0.f, 0.f, 0.f);
     TextRenderer->EnableBlending(true);
 
-    // Draw "Play" at center of button (0,0)
     TextRenderer->Label("Press S to Start", GL_VIEW_SIZE / 2, GL_VIEW_SIZE / 2, SimpleText::CENTER);
     TextRenderer->Label("Press O to quit", GL_VIEW_SIZE / 2, (GL_VIEW_SIZE / 2) + 20.f, SimpleText::CENTER);
 
@@ -97,13 +91,11 @@ void drawPause()
     glClearColor(0.f, 0.f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    // Configure TextRenderer
     TextRenderer->ResetFont();
     TextRenderer->SetColor(SimpleText::TEXT_COLOR, SimpleText::Color::WHITE);
     TextRenderer->SetColorf(SimpleText::BACKGROUND_COLOR, 0.f, 0.f, 0.f, 0.f);
     TextRenderer->EnableBlending(true);
 
-    // Draw "Play" at center of button (0,0)
     TextRenderer->Label("Press S to continue", GL_VIEW_SIZE / 2, GL_VIEW_SIZE / 2, SimpleText::CENTER);
     TextRenderer->Label("Press O to quit", GL_VIEW_SIZE / 2, (GL_VIEW_SIZE / 2) + 20.f, SimpleText::CENTER);
 
@@ -115,13 +107,11 @@ void drawPause()
 
 void drawUI(const int &score, const int &time)
 {
-    // Configure TextRenderer
     TextRenderer->ResetFont();
-    TextRenderer->SetColor(SimpleText::TEXT_COLOR, SimpleText::Color::WHITE);
+    TextRenderer->SetColor(SimpleText::TEXT_COLOR, SimpleText::Color::BLACK);
     TextRenderer->SetColorf(SimpleText::BACKGROUND_COLOR, 0.f, 0.f, 0.f, 0.f);
     TextRenderer->EnableBlending(true);
 
-    // Draw "Play" at center of button (0,0)
     TextRenderer->Label("Press R to Restart", 50.f, 40.f, SimpleText::CENTER);
     TextRenderer->Label("Press O to quit", 50.f, 20.f, SimpleText::CENTER);
 
